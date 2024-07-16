62c5cff63dfc894b5d7f0c49bb2c87b4ea9c853d
Sohan Fernando <sohanfernando7@gmail.com>
2024-07-16 16:42:52 +0530

    core.ts update

--- casecobra/src/app/configure/upload/page.tsx
+++ casecobra/src/app/configure/upload/page.tsx
@@ -1,17 +1,35 @@
 "use client"
 
+import { Progress } from "@/components/ui/progress";
+import { useToast } from "@/components/ui/use-toast";
+import { useUploadThing } from "@/lib/uploadthing";
 import { cn } from "@/lib/utils";
 import { Image, Loader2, MousePointerSquareDashed } from "lucide-react";
+import { useRouter } from "next/router";
 import { useState, useTransition } from "react";
 import Dropzone, { FileRejection } from "react-dropzone";
 
 const Page = () => {
-
+    const { toast } = useToast()
     const [isDragOver, setIsDragOver] = useState<boolean>(false)
+    const [ uploadProgress, setUploadProgress ] = useState<number>(0)
+    const router = useRouter()
+
+    const { startUpload } = useUploadThing("imageUploader", {
+        onClientUploadComplete: ([data]) => {
+            const configId = data.serverData.configId
+            startTransition(() => {
+                router.push(`/configure/design?id=${configId}`)
+            })
+        },
+        onUploadProgress(p) {
+            setUploadProgress(p)
+        },
+    })
 
     const onDropRejected = () => {}
-    const onDropAccepted = () => {
-        console.log("accepted")
+    const onDropAccepted = (acceptedFiles: File[]) => {
+        
     }
 
     const isUploading = false
@@ -37,9 +55,41 @@ const Page = () => {
                 {({getRootProps, getInputProps}) => (
                     <div className="h-full w-full flex-1 flex flex-col items-center justify-center" {...getRootProps()}>
                         <input {...getInputProps()} />
-                        {isDragOver ? <MousePointerSquareDashed className="h-6 w-6 text-zinc-500 mb-2"/> : isUploading || isPending ? <Loader2 className="animae-spin 
-                        h-6 w-6 text-zinc-500 mb-2"/> : <Image className="h-6 w-6 text-zinc-500 mb-2" />}
-                        <div className="flex flex-col justify-center mb-2 text-sm text-zinc-700"></div>
+                        {isDragOver ? (
+                            <MousePointerSquareDashed className='h-6 w-6 text-zinc-500 mb-2' />
+                        ) : isUploading || isPending ? (
+                            <Loader2 className='animate-spin h-6 w-6 text-zinc-500 mb-2' />
+                        ) : (
+                            <Image className='h-6 w-6 text-zinc-500 mb-2' />
+                        )}
+                        <div className="flex flex-col justify-center mb-2 text-sm text-zinc-700">
+                            {isUploading ? (
+                                <div className="flex flex-col items-center">
+                                    <p>Uploading...</p>
+                                    <Progress
+                                        value={uploadProgress}
+                                        className="mt-2 w-40 h-2 bg-gray-300"/>
+                                </div>
+                            ) : isPending ? (
+                            <div className="flex flex-col items-center">
+                                <p>Redirecting, please wait...</p>
+                            </div>
+                            ) : isDragOver ? (
+                                <p>
+                                    <span className="font-semibold">Drop file </span>
+                                    to upload
+                                </p>
+                            ) : (
+                                <p>
+                                    <span className="font-semibold">Click to upload </span>
+                                    or drag and drop
+                                </p>
+                            )}
+                        </div>
+
+                        {isPending ? null : (
+                            <p className="text-xs text-zinc-500">PNG, JPG, JPEG</p>
+                        )}
                     </div>
                 )}
             </Dropzone>

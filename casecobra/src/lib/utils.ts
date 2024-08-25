815ae197293bfa171c9864eb8b4148434d6a88bf
Sohan Fernando <sohanfernando7@gmail.com>
2024-08-25 18:28:25 +0530

    update utils.ts

--- casecobra/src/lib/utils.ts
+++ casecobra/src/lib/utils.ts
@@ -42,6 +42,6 @@ export function constructMetadata({
       creator: '@sohantriedcoding',
     },
     icons,
-    metadataBase: new URL("https://casecobra.vercel.app/")
+    metadataBase: new URL("https://casecobra-omega-liard.vercel.app/")
   }
 }
